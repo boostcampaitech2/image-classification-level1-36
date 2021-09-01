2324d469c68140e3fafc65a82d12d33a59ad1e50
root <shawn.khs1208@gmail.com>
2021-09-01 07:07:09 +0000

    update

---
+++ T2067/train.py
@@ -0,0 +1,15 @@
+#from utils import crop, dataloader, process_data
+import sys
+import os
+current_path = os.path.join(os.getcwd(),'T2067')
+sys.path.append(current_path)
+
+from models.define_model import CreateModel
+from utils.crop import *
+from utils.dataloader import *
+from utils.process_data import *
+
+#current_path = os.path.join(os.getcwd(),'T2067')
+
+dataframe = create_train_dataframe('data/train','cropped')
+model = CreateModel("efficientnet_b3",num_classes=18)
